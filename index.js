d7086ff994821eb6aa34b42dada1f23e9612c9fe
HJTsao <houngjohntsao@gmail.com>
2016-07-26 16:13:56 +0800

    Change position property and add link button animate.
    Due to some zooming problem, change some position property according to parent and child.
    And position be specified by pixel now.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 	$('.front img').fadeIn(1000,function(){
 		$('.front-text').css({'display':'block', 'opacity':'0'})
-					    .animate({'opacity':'1', 'left':'24%'},1000,greetingOut);
+					    .animate({'opacity':'1', 'left':'18%'},1000,greetingOut);
 	});
+
+	$('.icon').mouseenter(function(){
+		$(this).animate({'top':'-12%','opacity':'1'},{duration:200,queue:false})
+			   .mouseleave(function(){
+		 	   		$(this).animate({'top':'-6%','opacity':'0.8'},{duration:200,queue:false});
+				});
+		});
 });
 
+
+
+		
 var greetingOut = function(){
 	$('.front-text')
 					.animate({'opacity':'0'},500,linksIn);
@@ -14,5 +24,6 @@ var linksIn = function(){
 	$('.front-text').remove();
 	$('.links').css({'display':'block', 'opacity':'0'})
 			   .animate({'opacity':'1'},500);
+	$('.icon').animate({'top':'-6%','opacity':'0.8'},500);
 
 }
