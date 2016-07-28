49fb50153d47d6774cb3d3d75d9e9ce987887e88
HJTsao <houngjohntsao@gmail.com>
2016-07-28 16:17:35 +0800

    Add games I'm playing.

--- index.js
+++ index.js
@@ -11,29 +11,49 @@ $(document).ready(function(){
 				});
 		});
 	scrollSense();
-	seperate1Out();
+	seperate();
 	myPhoto();
+	buttonclick();
 });
 
 var scrollSense = function(){
 	$(window).scroll(function(){
 		var nowTop = $(this).scrollTop();
 		$('#scrollCount').remove();
-		$('<p id="scrollCount">' + nowTop + '</p>').appendTo($('.WhoAreYou .text-area .row'));
+		$('<p id="scrollCount">' + nowTop + '</p>').appendTo($('#scrollTop'));
 	});
 
+}
+
+var seperate = function(){
+	seperate1Out();
+	seperate2Out();
 }
 
 var seperate1Out = function(){
 	$(window).scroll(function(){
 		var nowTop = $(this).scrollTop();
-		if(nowTop >= 400){
+		if(nowTop >= 450){
 			$('#seperate1').animate({'opacity':'1','width':'300px'},{duration:400,queue:false});
 			$('.WhoAreYou h1').animate({'opacity':'1'},{duration:400,queue:false});
 		}
 		else{
 			$('#seperate1').animate({'opacity':'0','width':'0px'},{duration:400,queue:false});
 			$('.WhoAreYou h1').animate({'opacity':'0'},{duration:400,queue:false});
+		}
+	});
+}
+
+var seperate2Out = function(){
+	$(window).scroll(function(){
+		var nowTop = $(this).scrollTop();
+		if(nowTop >= 1600){
+			$('#seperate2').animate({'opacity':'1','width':'300px'},{duration:400,queue:false});
+			$('.whatGame h1').animate({'opacity':'1'},{duration:400,queue:false});
+		}
+		else{
+			$('#seperate2').animate({'opacity':'0','width':'0px'},{duration:400,queue:false});
+			$('.whatGame h1').animate({'opacity':'0'},{duration:400,queue:false});
 		}
 	});
 }
@@ -79,4 +99,39 @@ var linksIn = function(){
 			   .animate({'opacity':'1'},500);
 	$('.icon').animate({'top':'-6%','opacity':'0.8'},500);
 
+}
+
+var buttonclick = function(){
+	/*$('#dot1').click(function(){
+		$('.game2').fadeOut(500).removeClass('active-game');
+		$('.game1').fadeIn(500).addClass('active-game');
+		$('#dot2').removeClass('active-dot');
+		$(this).addClass('active-dot');
+	});*/
+
+	/*$('#dot1').click(function(){
+		$('.game2').fadeOut(500);
+		$('.game2').delay(500).removeClass('active-game').delay(500);
+		
+		$('.game1').delay(500).addClass('active-game').fadeIn(500);
+		$('#dot2').delay(230).removeClass('active-dot');
+		
+		$(this).delay(250).addClass('active-dot');
+	});*/
+	$('#dot1').click(function(){
+		$('.game2').fadeOut(500);
+		$('.game1').delay(500).fadeIn(500);
+		$('#dot2').delay(230).removeClass('active-dot');
+		
+		$(this).delay(250).addClass('active-dot');
+	});
+
+
+	$('#dot2').click(function(){
+		$('.game1').fadeOut(500);
+		$('.game2').delay(500).fadeIn(500);
+		$('#dot1').delay(230).removeClass('active-dot');
+		
+		$(this).delay(250).addClass('active-dot');
+	});
 }
